2812b65fb8b89e8897a278b84ecc12ca8167ed2a
mstaniford <mstaniford@users.noreply.github.com>
2022-08-19 11:56:53 +0100

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "tfStan"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
